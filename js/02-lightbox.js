cc170e259d5cc7272769c2b6ff27e83ebc07a2eb
KaterynaTesta <84970518+KaterynaTesta@users.noreply.github.com>
2021-10-21 22:59:13 +0400

    task-01

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallerySet = document.querySelector('.gallery');
+gallerySet.addEventListener('click', event);
+
+function renderGallery() {
+    const markUpGallerry = items
+        .map(
+            item =>
+                `<a class="gallery__item" href="${item.original}">
+                    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                </a>`,
+        )
+        .join('');
+    gallerySet.insertAdjacentHTML('beforeend', markUpGallerry);
+}
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionType: `alt`,
+    captionDelay: 250,
+});
